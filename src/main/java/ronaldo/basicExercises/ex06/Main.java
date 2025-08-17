67ab2fb8a3682ce62512f4ce848b093590577ce9
Ronaldo Ribeiro <filhoronaldo.ribeiro@gmail.com>
2025-08-17 01:00:22 -0300

    Create Main.java

---
+++ src/main/java/ronaldo/basicExercises/ex06/Main.java
@@ -0,0 +1,11 @@
+package ronaldo.basicExercises.ex06;
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 3) {
+            System.out.println((i+1)+"°) Hello World");
+            i++;
+        }
+    }
+}
