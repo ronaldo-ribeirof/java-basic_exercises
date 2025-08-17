ad6de658cdb6168fd14225b6566f864277df394d
Ronaldo Ribeiro <filhoronaldo.ribeiro@gmail.com>
2025-08-16 22:12:02 -0300

    refactor: finished my own print methods
    * Introduced two new methods, made by me, printMatrixI and printMatrixII, for printing float matrices with different formats.
    
    Updated main to use my two methods and also the Matrixnator API, for comparison purposes

--- src/main/java/ronaldo/matrix/ex01/Main.java
+++ src/main/java/ronaldo/matrix/ex01/Main.java
@@ -1,9 +1,32 @@
 package ronaldo.matrix.ex01;
 
 import java.util.Scanner;
+import java.util.Arrays;
 import ronaldo.matrix.Matrixnator;
 
 public class Main {
+    public static void printMatrixI(float[][] m) {
+        int numLinhas = m.length;
+        int numColunas = m[0].length;
+
+        for (int l = 0; l < numLinhas; l++) {
+            System.out.print("|");
+            for (int c = 0; c < numColunas; c++) {
+                System.out.printf(" %3.2f |", m[l][c]);
+            }
+            System.out.println();
+        }
+        System.out.println("Printed by Me.");
+    }
+
+    public static void printMatrixII(float[][] m) {
+        int numLinhas = m.length;
+        for (int l = 0; l < numLinhas; l++) {
+            System.out.println(Arrays.toString(m[l]));
+        }
+        System.out.println("Printed by Me.");
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -15,12 +38,17 @@ public class Main {
 
         float[][] matrix = new float[qtdeLinhas][qtdeColunas];
 
-        for (int l = 0; l < qtdeLinhas; l++) {
-            for (int c = 0; c < qtdeColunas; c++) {
-                System.out.printf("Digite o valor do elem. da linha %d, coluna %d: ", l+1, c+1);
-                matrix[l][c] = Float.parseFloat(scanner.nextLine());
+        for (int linha = 0; linha < qtdeLinhas; linha++) {
+            for (int coluna = 0; coluna < qtdeColunas; coluna++) {
+                System.out.printf("Digite o valor do elem. da linha %d, coluna %d: ", linha+1, coluna+1);
+                matrix[linha][coluna] = Float.parseFloat(scanner.nextLine());
             }
         }
         Matrixnator.printFloatMatrix(matrix);
+        System.out.println();
+        printMatrixI(matrix);
+        System.out.println();
+        printMatrixII(matrix);
+
     }
 }
