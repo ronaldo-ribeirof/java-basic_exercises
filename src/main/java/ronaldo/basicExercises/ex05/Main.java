4b66c3e8e7800e7fabd9a276b58e4a2ca6d07d3a
Ronaldo Ribeiro <filhoronaldo.ribeiro@gmail.com>
2025-08-17 00:25:33 -0300

    feat: add ex05
    Learned about switch in java and made a reference for Game of Thrones!

---
+++ src/main/java/ronaldo/basicExercises/ex05/Main.java
@@ -0,0 +1,30 @@
+package ronaldo.basicExercises.ex05;
+
+/*
+Aprendi sobre o switch!
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("The");
+
+        int season = 4;
+        switch (season) {
+            case 1:
+                System.out.println("Spring");
+                break;
+            case 2:
+                System.out.println("Summer");
+                break;
+            case 3:
+                System.out.println("Fall");
+                break;
+            case 4:
+                System.out.println("Winter");
+                break;
+        }
+
+        System.out.println("is coming!");
+
+    }
+}
